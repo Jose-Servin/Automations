dd50b766033fb9f3492f25924867b1c96d04508c
Jose Servin <Servinjose96@gmail.com>
2022-01-21 10:38:41 -0600

    new child class Phone created

--- OOP/main.py
+++ OOP/main.py
@@ -49,3 +49,25 @@ class Item:
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
+class Phone(Item):
+    all = []
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+        # Run validations to recieved arguments
+        assert price >= 0, f"Price {price} is not >= zero."
+        assert quantity >= 0, f"Quantity {quantity} is not >= zero."
+        assert broken_phones >= 0, f"Broken Phones {broken_phones} is not >= zero."
+
+        # Assign to self object
+        self.name  = name
+        self.price = price
+        self.quantity = quantity 
+        self.broken_phones = broken_phones
+
+        # Actions to execute
+        Phone.all.append(self)
+
+phone1 = Phone('ServinV10', 500, 5, 1)
+phone2 = Phone('ServinV20', 700, 5, 1)
+
+print(phone1.calculate_total_price())
+print(phone2.calculate_total_price())
