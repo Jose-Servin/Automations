38514f8373b7d45db9fbe1216c7df83b3dc6c9f9
Jose Servin <Servinjose96@gmail.com>
2022-01-21 13:43:10 -0600

    finished notes on OOP YouTube video one

--- OOP/item.py
+++ OOP/item.py
@@ -10,21 +10,38 @@ class Item:
 
         # Assign to self object
         self.__name  = name
-        self.price = price
+        self.__price = price
         self.quantity = quantity 
 
         # Actions to execute
         Item.all.append(self)
 
     @property
+    def price(self):
+        return self.__price
+
+    def apply_discount(self):
+        self.__price = self.__price * self.pay_rate
+
+    def apply_increment(self, increment_value):
+        self.__price = self.__price + self.__price * increment_value
+
+    @property
+    # property decorator = Read-Only Attribute 
     def name(self):
         return self.__name
 
-    def calculate_total_price(self):
-        return self.price * self.quantity
+    @name.setter
+    def name(self, value):
+        if len(value) > 10:
+            raise Exception('The name is too long')
+        else:
+            self.__name = value
 
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
+    def calculate_total_price(self):
+        return self.__price * self.quantity
+
+
 
     @classmethod
     def instantiate_from_csv(cls):
@@ -49,4 +66,4 @@ class Item:
             return False
 
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}('{self.name}', {self.__price}, {self.quantity})"
--- OOP/main.py
+++ OOP/main.py
@@ -5,5 +5,11 @@ from item import Item
 
 item1 = Item('MyItem', 750)
 #item1.name = 'Other Name' # will return an AttributeError because of the name property defined
-
+item1.name = 'Other Name' # after using a setter, no errors returned
 print(item1.name)
+
+print(item1.price)
+item1.apply_increment(0.2)
+print(item1.price)
+item1.apply_discount()
+print(item1.price)
