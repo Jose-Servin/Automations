e815339e28c6ff1cb233a0a5a0320494dd42e9dc
Jose Servin <Servinjose96@gmail.com>
2022-01-21 16:37:54 -0600

    OOP project creation

---
+++ OOP_Servin/SoftwareEngineer.py
@@ -0,0 +1,9 @@
+class SoftwareEngineer():
+
+    def __init__(self, name, age, level, salary):
+        # these are instance attributes
+        self.name = name
+        self.age = age
+        self.level = level
+        self.salary = salary
+
---
+++ OOP_Servin/oop1.py
@@ -0,0 +1,8 @@
+# Representing Software Engineers
+from curses.ascii import SO
+from SoftwareEngineer import SoftwareEngineer
+# position, name, age, level, salary
+se1 = SoftwareEngineer('Jose',25,'Junior', 7000)
+se2 = ['Software Engineer', 'Baker', 9, 'Senior', 9000]
+
+print(se1.name)
