71676d207179622fb00d9e8a1dbdae77680ecd03
Jose Servin <Servinjose96@gmail.com>
2022-01-22 12:23:01 -0600

    new OOP notes on 4 principles completed

--- OOP_Servin/SoftwareEngineer.py
+++ OOP_Servin/SoftwareEngineer.py
@@ -1,27 +1,64 @@
 
 class Employee:
-    def __init__(self, name, age):
+    def __init__(self, name, age, salary):
         self.name = name
         self.age = age
+        self.salary = salary
+        self._ssn = None
         
+    def work(self):
+        print(f'{self.name} is working')
 
-class SoftwareEngineer():
+    def reward(self):
+        print(f"Thank you for the hard work {self.name}, here are some free coffee gift cards! ")
+
+    def __str__(self):
+        information = f"name: {self.name}, age: {self.age}, salary: {self.salary}"
+        return information
+
+    def motivate(employees):
+        for i in employees:
+            i.reward()
+
+    # getter
+    def get_ssn(self):
+        return self._ssn
+
+    # setter (allows us to check value, enforce constraints etc)
+    def set_ssn(self, ssn):
+        self._ssn = self._special_emp(ssn)
+
+    def _special_emp(self, ssn):
+        if ssn <= 555555:
+            print('Special Employee')
+        else:
+            print('Regular Employee')
+
+
+class SoftwareEngineer(Employee):
 
     # Class attribute
     alias = 'Keyboard Magician'
-    def __init__(self, name, age, level, salary):
+    def __init__(self, name, age, salary, level):
+        super().__init__(name, age, salary)
         # these are instance attributes
-        self.name = name
-        self.age = age
         self.level = level
-        self.salary = salary
+        self._log_time = 0
 
     # this is an instance method
     def code(self):
         print(f"{self.name} is writting code...")
+        self._log_time += 1 
 
     def code_in_language(self, language):
         print (f"{self.name} is writting code in {language}.")
+
+    def work(self):
+        print(f'{self.name} is working on some code for Database MGMT')
+
+    def reward(self):
+        print(f"Thank you for the hard work {self.name}, here is a new keyboard! ")
+
 
     #def information(self):
     #    information = f"name: {self.name}, age: {self.age}, level: {self.level}"
@@ -31,7 +68,7 @@ class SoftwareEngineer():
 
     # __str__ method is used to output all members of the class
     def __str__(self):
-        information = f"name: {self.name}, age: {self.age}, level: {self.level}"
+        information = f"name: {self.name}, age: {self.age}, salary: {self.salary}, level: {self.level}"
         return information
 
     # by default __eq__ compares the memory address of an object
@@ -51,3 +88,23 @@ class SoftwareEngineer():
             return 7000
         if age < 40:
             return 9000
+
+
+class Designer(Employee):
+    def __init__(self, name, age, salary, studio_location):
+        super().__init__(name, age, salary)
+        self.studio = studio_location
+    
+    def at_studio(self):
+        print(f"{self.name} is at the studio located in {self.studio}")
+
+    def work(self):
+        print(f'{self.name} is working on a new design for the landing page.')
+
+    def __str__(self):
+        information = f"name: {self.name}, age: {self.age}, salary: {self.salary}, studio: {self.studio}."
+        return information
+
+    def reward(self):
+        print(f"Thank you for the hard work {self.name}, here are some new art supplies!")
+
--- OOP_Servin/main.py
+++ OOP_Servin/main.py
@@ -1,12 +1,12 @@
 # Representing Software Engineers
-from curses.ascii import SO
-from re import S
+from SoftwareEngineer import Designer
+from SoftwareEngineer import Employee
 from SoftwareEngineer import SoftwareEngineer
 # position, name, age, level, salary
 
 # these represent instances of the Class SoftwareEngineer
-se1 = SoftwareEngineer('Jose',25,'Junior', 7000)
-se2 = SoftwareEngineer('Baker', 9, 'Senior', 9000)
+se1 = SoftwareEngineer('Jose',25, 7000, 'Junior')
+se2 = SoftwareEngineer('Baker', 9, 9000, 'Senior')
 
 print(se1.name)
 print(se1.alias)
@@ -20,9 +20,43 @@ se2.code_in_language('SQL')
 print(se1) # without overidding the __str__ method this returns an object 
 print(se2)
 
-se3 = SoftwareEngineer('Camila', 8, 'Senior', 10000)
+se3 = SoftwareEngineer('Camila', 8, 9000, 'Senior',)
 print(se3 == se2)
 
 
 # Calling the Class Method
 print(SoftwareEngineer.entry_salary(27))
+
+print(se3.level)
+
+designer = Designer('Bella', 8, 1200, 'Houston')
+designer.at_studio()
+print(designer)
+#designer.code() # returns error because designer instance created cannot code 
+
+general = Employee('Tom', 45, 3000)
+general.work() # work function from Parent class level 
+se1.work() # work function override in the child class level
+designer.work()
+
+
+employees = [se1, se2, se3, general, designer]
+
+#Polymorphism example 
+Employee.motivate(employees)
+
+print(se1.salary)
+
+karen = Employee('Karen', 50, 4500)
+karen.set_ssn(555555)
+print(karen.get_ssn())
+
+se1.code()
+print(se1._log_time)
+se1.code()
+print(se1._log_time)
+
+
+m = Employee('m', 50, 8000)
+m.set_ssn(999999)
+print(m.get_ssn())
