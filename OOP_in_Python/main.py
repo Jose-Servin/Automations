dd0d7d8e275eb6f644f8ff60fcabdf11e2811fcf
Jose Servin <Servinjose96@gmail.com>
2022-01-27 15:09:13 -0600

    new inner class concept check and notes

--- OOP_in_Python/main.py
+++ OOP_in_Python/main.py
@@ -1,12 +1,49 @@
+
 class Car:
+
+    # Class variables are called Static Variables 
+    wheels = 4
+
     # idea behind __init__ is that this method will get called once automatically per object 
     def __init__(self, color, year, make, model ): # grey 2021 Honda Accord 
         self.color = color
         self.year = year
         self.make = make
         self.model = model
+        self.insurance = self.Insurance() #creating instance of inner class in outer class 
 
     def details(self):
         print(f'Your vehicle info is {self.color}, {self.year}, {self.make}, {self.model}.')
 
+
+    # Accessor Methods: fetch value of instance variable 
+    def get_year(self):
+        return self.year
+
+    # Mutator Methods: set new values to instance variable 
+    def set_color(self, new_color):
+        self.color = new_color
+
+    @classmethod
+    def wheel_info(cls):
+        return cls.wheels
+    
+    # static methods do not take concern over Class or Instances 
+    @staticmethod
+    def info():
+        print('This method has nothing to do with Class or Instances')
+
+
+    def show_info(self):
+        print(f'This car is a {self.year}, {self.make} {self.model}. ')
+        self.insurance.show_info() # in the outter class, call the inner class function 
+
+    
+    class Insurance:
+        def __init__(self, company='StateFarm', rate=165):
+            self.company = company
+            self.rate = rate
+
+        def show_info(self):
+            print(f'Your insurance company is {self.company} and you pay ${self.rate} per month.')
 
--- OOP_in_Python/run.py
+++ OOP_in_Python/run.py
@@ -2,3 +2,49 @@ from main import Car
 
 my_car = Car('sonic grey', 2021, 'Honda', 'Accord')
 my_car.details()
+
+my_car2 = Car('grey',2015,'Chevy','Malibu')
+
+# Testing to see how Static variables/Class variables are also a part of individual instances
+print(my_car.wheels)
+print(my_car2.wheels)
+
+Car.wheels = 5 # to modify Class Variables you use the Class namespace
+print(my_car.wheels)
+print(my_car2.wheels)
+
+Car.wheels = 6
+
+# Testing the Class Method created 
+print(Car.wheel_info())
+
+# Testing the static method 
+Car.info()
+
+# Testing show_info method
+my_car.show_info()
+
+# Accessing the inner class Insurance variables
+print(my_car.insurance.company)
+
+# Creating instance (object) for inner class
+car1_insurance = my_car.insurance
+print(car1_insurance.rate)
+
+# Creating instance of inner class outside the outer class
+default_insurance = Car.Insurance()
+print(default_insurance.company)
+
+# Creating instance of inner class outside the outer class with non-default arguments
+high_insurance = Car.Insurance('Geiko', 200)
+print(high_insurance.rate)
+
+# Creating instance of inner class via Outter class with non-default arguments
+my_car3 = Car('Red',2010, 'Chevy','Trailblazer')
+car3_insurance = my_car3.insurance
+car3_insurance.company = 'TAMU Insurance'
+car3_insurance.rate = 1212
+
+print(car3_insurance.rate, car3_insurance.company)
+
+my_car3.show_info()
