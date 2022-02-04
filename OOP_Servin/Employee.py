5c22003640db6067dffb719462c97590faa98dc5
Jose Servin <Servinjose96@gmail.com>
2022-02-03 20:51:44 -0600

    new child class added to Employee and CSV instantiate function added to main_employee run file

--- OOP_Servin/Employee.py
+++ OOP_Servin/Employee.py
@@ -1,6 +1,11 @@
+import csv 
+import pandas as pd
+
 class Employee:
 
-    def __init__(self, first_name, last_name, ID, department, salary, bonus, start_date, end_date:None):
+    all_employees = []
+
+    def __init__(self, first_name, last_name, ID, department, salary, bonus, start_date, end_date = None):
         self.first_name  = first_name
         self.last_name = last_name
         self.ID = ID
@@ -10,5 +15,72 @@ class Employee:
         self.start_date = start_date
         self.end_date = end_date
 
+        Employee.all_employees.append(self)
+
     def total_compensation(self):
         return self.salary + (self.salary * self.bonus) 
+
+    def __str__(self):
+        return f""" 
+        Employee Report: \n
+        First Name: {self.first_name}
+        Last Name: {self.last_name}
+        ID: {self.ID}
+        Department: {self.department}
+        Salary: {self.salary}
+        Bonus: {self.bonus} 
+        Start Date: {self.start_date} 
+        End Date: {self.end_date} 
+        
+        """
+
+    def __repr__(self):
+        return f""" 
+        Employee Report: 
+        \t First Name: {self.first_name} 
+        \t Last Name: {self.last_name}
+        \t ID: {self.ID} 
+        \t Department: {self.department}
+        \t Salary: {self.salary}
+        \t Bonus: {self.bonus}
+        \t Start Date: {self.start_date}
+        \t End Date: {self.end_date} 
+        
+        """
+
+
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('/Users/joseservin/Automations/OOP_Servin/employees.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Employee(
+                first_name=item.get('first_name'),
+                last_name=item.get('last_name'),
+                ID=int(item.get('ID')),
+                department= item.get('Department'),
+                salary = int(item.get('Salary')),
+                bonus = float(item.get('bonus')),
+                start_date = pd.to_datetime(item.get('start_date')),
+                end_date = pd.to_datetime(item.get('end_date'))
+
+            )
+
+
+
+class Analyst(Employee):
+    def __init__(self, first_name, last_name, ID, department, salary, bonus, start_date, end_date =  None):
+        super().__init__(first_name, last_name, ID, department, salary, bonus, start_date, end_date)
+
+class Sales(Employee):
+    def __init__(self, first_name, last_name, ID, department, salary, bonus, start_date, end_date=None):
+        super().__init__(first_name, last_name, ID, department, salary, bonus, start_date, end_date)
+
+class HR(Employee):
+    def __init__(self, first_name, last_name, ID, department, salary, bonus, start_date, end_date=None):
+        super().__init__(first_name, last_name, ID, department, salary, bonus, start_date, end_date)
+
--- OOP_Servin/main_employee.py
+++ OOP_Servin/main_employee.py
@@ -1,4 +1,11 @@
 from Employee import Employee
 
-employee_1 = Employee('Jose','Servin',880333,'Analyst', 10000, 0.03, '2021-06-01', None)
-print(employee_1.total_compensation())
+#employee_1 = Employee('Jose','Servin',880333,'Analyst', 10000, 0.03, '2021-06-01', None)
+#print(employee_1.total_compensation())
+
+all = Employee.instantiate_from_csv()
+print(Employee.all_employees)
+
+for employee in Employee.all_employees:
+    print(employee.total_compensation())
+    print(employee.department)
