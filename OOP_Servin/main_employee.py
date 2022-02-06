a52d73138a4fbf6636bf231dd35aa8d9b429be59
Jose Servin <Servinjose96@gmail.com>
2022-02-06 11:29:02 -0600

    testing main.py file in PyCharm

--- OOP_Servin/main_employee.py
+++ OOP_Servin/main_employee.py
@@ -1,7 +1,9 @@
 from Employee import Employee
 
-#employee_1 = Employee('Jose','Servin',880333,'Analyst', 10000, 0.03, '2021-06-01', None)
-#print(employee_1.total_compensation())
+# Reformatting code in PyCharm
+
+# employee_1 = Employee('Jose','Servin',880333,'Analyst', 10000, 0.03, '2021-06-01', None)
+# print(employee_1.total_compensation())
 
 all = Employee.instantiate_from_csv()
 print(Employee.all_employees)
